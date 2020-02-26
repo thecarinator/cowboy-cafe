31749162b4485cee301dc545d075c9e33e1249e8
thecarinator <thecarinator@F1092-10-2098>
2020-02-26 14:17:57 -0600

    f

---
+++ Data/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace CowboyCafe.Data
+{
+    public class Order: INotifyPropertyChanged
+    {
+        private uint lastOrderNumber;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<IOrderItem> items;
+        public IEnumerable<IOrderItem> Items => items.ToArray();
+        public double Subtotal { get; }
+        public uint OrderNumber { get; }
+
+        public void Add(IOrderItem item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
+        public void Remove(IOrderItem item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+    }
+}
---
+++ DataTests/OrderTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CowboyCafe.Data;
+using Xunit;
+
+namespace CowboyCafe.DataTests
+{
+    public class OrderTests
+    {
+        class MockOrderItem: IOrderItem
+        {
+            public double Price { get; }
+            public List<string> SpecialInstructions { get; }
+        }
+        // Adding something to the order should
+        // have it appear in the Items property
+        [Fact]
+        public void AddedIOrderItemsAppearInItemsProperty()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            Assert.Contains(item, order.Items);
+        }
+
+        public void RemovedORderITemDoesNotAppearInItemsProperty()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            order.Remove(item);
+            Assert.DoesNotContain(item, order.Items);
+        }
+
+        // Removing something from the order should
+        // remove it from the Items property
+
+        // Get the price - needs to be right
+        // (For the items we've added)
+    }
+}
