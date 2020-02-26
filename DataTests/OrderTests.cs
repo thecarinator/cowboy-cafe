00b7959408bfa36a966b0c93ad8ebf5ac803c89c
thecarinator <thecarinator@F1092-10-2098>
2020-02-26 14:45:03 -0600

    d

--- DataTests/OrderTests.cs
+++ DataTests/OrderTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using CowboyCafe.Data;
 using Xunit;
 
@@ -10,7 +11,7 @@ namespace CowboyCafe.DataTests
     {
         class MockOrderItem: IOrderItem
         {
-            public double Price { get; }
+            public double Price { get; set; }
             public List<string> SpecialInstructions { get; }
         }
         // Adding something to the order should
@@ -24,13 +25,54 @@ namespace CowboyCafe.DataTests
             Assert.Contains(item, order.Items);
         }
 
-        public void RemovedORderITemDoesNotAppearInItemsProperty()
+        [Fact]
+        public void RemovedOrderITemDoesNotAppearInItemsProperty()
         {
             var order = new Order();
             var item = new MockOrderItem();
             order.Add(item);
             order.Remove(item);
             Assert.DoesNotContain(item, order.Items);
+        }
+
+        [Theory]
+        [InlineData(new double[] { })]
+        [InlineData(new double[] { 0 })]
+        [InlineData(new double[] { 10, 15, 18 })]
+        [InlineData(new double[] { 20, -4, 3.6, 8})]
+        [InlineData(new double[] { -100, -5})]
+        public void SubtotalShouldBeTheSumOfOrderItemPrices(double[] prices)
+        {
+            var order = new Order();
+            double total = 0;
+            foreach(var price in prices)
+            {
+                total += price;
+                order.Add(new MockOrderItem() {
+                    Price = price
+                });
+            }
+        }
+
+        [Fact]
+        public void ItemsShouldContainOnlyAddedItems()
+        {
+            var items = new IOrderItem[]
+            {
+                new MockOrderItem() {Price = 3 },
+                new MockOrderItem() {Price = 4 },
+                new MockOrderItem() {Price = 7}
+            };
+            var order = new Order();
+            foreach(var item in items)
+            {
+                order.Add(item);
+            }
+            Assert.Equal(items.Length, order.Items.Count());
+            foreach(var item in items)
+            {
+                Assert.Contains(item, order.Items);
+            }
         }
 
         // Removing something from the order should
